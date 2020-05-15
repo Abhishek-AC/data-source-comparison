d2f03970dff361174b32eb95127e57be50e173c7
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-05-14 19:29:25 -0700

    [ADD] pre-processing files

---
+++ pre-processing/convert.py
@@ -0,0 +1,18 @@
+import json
+with open('./links.json') as handle:
+    linksDump = json.loads(handle.read())
+with open('./converted.json') as same:
+    convertedDump = json.loads(same.read())
+    
+newDict = []
+for i in range(0,len(linksDump)):
+    temp = convertedDump[i]
+    temp["social_profiles"] = linksDump[i]
+    newDict.append(temp)
+
+with open('data.json', 'w') as outfile:
+    json.dump(newDict, outfile)
+
+
+
+
